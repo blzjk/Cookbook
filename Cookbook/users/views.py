14cff0b3f023d66c2ce68a822c7fb591ef9f1298
Blazej <b.kzkwski@gmail.com>
2021-11-12 09:55:24 +0100

    Panel admina i baza danych Działa rejestracja użytkownika

--- Cookbook/users/views.py
+++ Cookbook/users/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from .form import MyForm
 
+@csrf_exempt
 def register(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
     form = MyForm()
     return render(request=request, template_name='users/register.html',
                   context={
