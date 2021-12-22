6f2408083b688711572e6a63c6cda919f617ea9d
Blazej <b.kzkwski@gmail.com>
2021-12-22 21:06:35 +0100

    Udostępnianie przepisu

--- Cookbook/Cookbook/settings.py
+++ Cookbook/Cookbook/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'django_social_share'
 ]
 
 MIDDLEWARE = [
